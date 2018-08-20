64904bd7bdc25eb60bae629be52ff59f1ae134f7
Mike Falzerano <mfalzera@yahoo.com>
2018-08-19 23:37:12 -0400

    Commit after completing Chapter 5 of the tutorial.

--- 02_01b/app/media-item-form.component.js
+++ 02_01b/app/media-item-form.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core', '@angular/forms'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/forms', './media-item.service', './providers'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,10 @@ System.register(['@angular/core', '@angular/forms'], function(exports_1, context
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, forms_1;
+    var __param = (this && this.__param) || function (paramIndex, decorator) {
+        return function (target, key) { decorator(target, key, paramIndex); }
+    };
+    var core_1, forms_1, media_item_service_1, providers_1;
     var MediaItemFormComponent;
     return {
         setters:[
@@ -19,20 +22,29 @@ System.register(['@angular/core', '@angular/forms'], function(exports_1, context
             },
             function (forms_1_1) {
                 forms_1 = forms_1_1;
+            },
+            function (media_item_service_1_1) {
+                media_item_service_1 = media_item_service_1_1;
+            },
+            function (providers_1_1) {
+                providers_1 = providers_1_1;
             }],
         execute: function() {
             MediaItemFormComponent = (function () {
-                function MediaItemFormComponent() {
+                function MediaItemFormComponent(formBuilder, mediaItemService, lookupLists) {
+                    this.formBuilder = formBuilder;
+                    this.mediaItemService = mediaItemService;
+                    this.lookupLists = lookupLists;
                 }
                 MediaItemFormComponent.prototype.ngOnInit = function () {
-                    this.form = new forms_1.FormGroup({
-                        medium: new forms_1.FormControl('Movies'),
-                        name: new forms_1.FormControl('', forms_1.Validators.compose([
+                    this.form = this.formBuilder.group({
+                        medium: this.formBuilder.control('Movies'),
+                        name: this.formBuilder.control('', forms_1.Validators.compose([
                             forms_1.Validators.required,
                             forms_1.Validators.pattern('[\\w\\-\\s\\/]+')
                         ])),
-                        category: new forms_1.FormControl(''),
-                        year: new forms_1.FormControl('', this.yearValidator)
+                        category: this.formBuilder.control(''),
+                        year: this.formBuilder.control('', this.yearValidator)
                     });
                 };
                 MediaItemFormComponent.prototype.yearValidator = function (control) {
@@ -55,15 +67,16 @@ System.register(['@angular/core', '@angular/forms'], function(exports_1, context
                     }
                 };
                 MediaItemFormComponent.prototype.onSubmit = function (mediaItem) {
-                    console.log(mediaItem);
+                    this.mediaItemService.add(mediaItem);
                 };
                 MediaItemFormComponent = __decorate([
                     core_1.Component({
                         selector: 'mw-media-item-form',
                         templateUrl: 'app/media-item-form.component.html',
                         styleUrls: ['app/media-item-form.component.css']
-                    }), 
-                    __metadata('design:paramtypes', [])
+                    }),
+                    __param(2, core_1.Inject(providers_1.lookupListToken)), 
+                    __metadata('design:paramtypes', [forms_1.FormBuilder, media_item_service_1.MediaItemService, Object])
                 ], MediaItemFormComponent);
                 return MediaItemFormComponent;
             }());
